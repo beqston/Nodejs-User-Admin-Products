df73db5c46c7c6aae632e702a4c1e2e1ee0f09e3
Beqston <bako.rap@gmail.com>
2025-10-02 17:00:48 +0400

    add change role in admin panel

--- conntrolers/adminController.js
+++ conntrolers/adminController.js
@@ -114,3 +114,33 @@ export const updateUser = async (req, res) => {
   }
 }
 
+export const deleteUser =  async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deletedUser = await User.findByIdAndDelete(id);
+
+    if (!deletedUser) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'User not found!'
+      });
+    }
+
+    // If this is an API call from fetch, do NOT redirect.
+    return res.status(202).json({
+      status: 'success',
+      message: 'User deleted successfully'
+    });
+
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    return res.status(500).json({
+      status: 'error',
+      message: 'Internal server error'
+    });
+  }
+}
+
+
+
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,7 @@
 import expres from 'express';
 import User from '../models/user.js';
 import isAdminHelper, { isAdmin } from '../middleware/isAdmin.js';
-import { getAllUsers, getEditUser, getProducts, updateUser } from '../conntrolers/adminController.js';
+import { deleteUser, getAllUsers, getEditUser, getProducts, updateUser } from '../conntrolers/adminController.js';
 import upload from '../utils/multer.js';
 
 const adminRouter = expres.Router();
@@ -20,33 +20,7 @@ adminRouter.route('/admin/user/:id/edit')
   .get(getEditUser)
   .patch(uploadImage.single('image'), updateUser);
 
-adminRouter.delete('/admin/user/:id/delete', async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const deletedUser = await User.findByIdAndDelete(id);
-
-    if (!deletedUser) {
-      return res.status(404).json({
-        status: 'fail',
-        message: 'User not found!'
-      });
-    }
-
-    // If this is an API call from fetch, do NOT redirect.
-    return res.status(202).json({
-      status: 'success',
-      message: 'User deleted successfully'
-    });
-
-  } catch (error) {
-    console.error('Error deleting user:', error);
-    return res.status(500).json({
-      status: 'error',
-      message: 'Internal server error'
-    });
-  }
-});
+adminRouter.delete('/admin/user/:id/delete',deleteUser);
 
 
 adminRouter.get('/admin/products', getProducts)
